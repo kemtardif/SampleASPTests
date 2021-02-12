677d00a9913868674692f936e744e9bcaa729400
Kem Tardif <tardif.kremlin@gmail.com>
2021-02-12 09:32:23 -0500

    Some simple tests

---
+++ SimpleApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SimpleApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+using Xunit;
+using Moq;
+using System.Collections.Generic;
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            //Arrange
+            Product[] testData = new Product[] {
+                 new Product { Name = "Kayak", Price = 275M },
+                 new Product { Name = "Lifejacket", Price = 48.95M}
+                 };
+            var mock = new Mock<IDataSource>();
+
+            var controller = new HomeController();
+            mock.SetupGet(m => m.Products).Returns(testData);
+            controller.dataSource = mock.Object;
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model
+                as IEnumerable<Product>;
+
+            // Assert
+            Assert.Equal(testData, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
+                    && p1.Price == p2.Price));
+            mock.VerifyGet(m => m.Products, Times.Once);
+        }
+    }
+}
---
+++ SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SimpleApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IDataSource dataSource = new ProductDataSource();
+
+        public ViewResult Index()
+        {
+            return View(dataSource.Products);
+        }
+    }
+}
